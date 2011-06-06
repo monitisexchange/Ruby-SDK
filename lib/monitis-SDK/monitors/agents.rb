0996df2ad29b462803e6166c3c3c778197a6e873
Ankur <ankur.jain24@gmail.com>
2011-06-07 03:52:01 +0530

    defined agents API

---
+++ lib/monitis-SDK/monitors/agents.rb
@@ -0,0 +1,28 @@
+class Agents < Base 
+  
+  def getAgents(key)
+    options={:keyRegExp=>key}
+    get("agents",key)
+  end
+  
+  def getAgentInfo(agentId,loadTests)
+    options={:agentId=>agentId,:loadTests=>loadTests}
+    get("agentInfo",options)
+  end
+  
+  def getAllAgentsSnapshot(platform=nil,tag=nil)
+    options={:platform=>platform,:tag=>tag,:timezone=>Time.now.gmt_offset/60}
+    get("allAgentsSnapshot",options)
+  end
+  
+  def getAgentSnapshot(agentKey)
+    options={:agentKey=>agentKey,:timezone=>Time.now.gmt_offset/60}
+    get("agentSnapshot",options)    
+  end
+  
+  def deleteAgents(agentIds,keyRegExp)
+    options={:agentIds=>agentIds,:keyRegExp=>keyRegExp}
+    post("deleteAgents",options)
+  end
+  
+end
--- lib/monitis-SDK/monitors/drive_monitors.rb
+++ lib/monitis-SDK/monitors/drive_monitors.rb
@@ -1,52 +1,32 @@
 class DriveMonitors
 
   def  addMonitor(options={})
-	options.merge!(@default_options)
-	options[:action]="addDriveMonitor"
-	pp options
-	res=HTTParty.post("http://www.monitis.com/api",:body=>options)
-	Crack::JSON.parse(res)
+  	post("addDriveMonitor",options)
   end
 
   def editMonitor(options={})
-    options.merge!(@default_options)
-    options[:action]="editDriveMonitor"
-    pp options
-    res=HTTParty.post("http://www.monitis.com/api",:body=>options)
-    Crack::JSON.parse(res)
+    post("editDriveMonitor",options)
   end
 
  def getMonitors(agentId)
-    query=@default_options
-    query.merge!({:action=>"agentDrive",:agentId=>agentId})
-    options={:query=>query}
-    pp query
-    HTTParty.get("http://www.monitis.com/api",options)
+    options={:agentId=>agentId}
+    get("agentDrive",options)
   end
 
 
  def getMonitorInfo(monitorId )
-    query=@default_options
-    query.merge!({:action=>"driveInfo",:monitorId =>monitorId })
-    options={:query=>query}
-    pp query
-    HTTParty.get("http://www.monitis.com/api",options)
+    options={:monitorId =>monitorId }
+    get("getMonitorInfo",options)
   end
 
 def getResults(monitorId,day,month,year)
- query=@default_options
-    query.merge!({:action=>"driveResult",:monitorId=>monitorId,:day=>day,:month=>month,:year=>year,:timezone=>Time.now.gmt_offset/60})
-    options={:query=>query}
-    pp query
-    HTTParty.get("http://www.monitis.com/api",options)
+    options={:monitorId=>monitorId,:day=>day,:month=>month,:year=>year,:timezone=>Time.now.gmt_offset/60}
+    get("driveResult",options)
 end
 
   def getTopResults(limit=10,tag="Default")
-    query=@default_options
-    query.merge!({:action=>"topdrive",:limit=>limit,:tag=>tag,:timezone=>Time.now.gmt_offset/60})
-    options={:query=>query}
-    pp query
-    HTTParty.get("http://www.monitis.com/api",options)
+    options={:limit=>limit,:tag=>tag,:timezone=>Time.now.gmt_offset/60}
+    get("topdrive",options)
   end
  end
 
