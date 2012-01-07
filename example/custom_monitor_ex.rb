b08df00cdbe6942838ebb498c7d6f8997b6ed926
Casey Strouse <casey.strouse@gmail.com>
2012-01-06 20:02:53 -0700

    Removed example file that wasn't supposed to be pushed

--- example/custom_monitor_ex.rb
+++
@@ -1,8 +0,0 @@
-require File.join('..','lib','monitis-SDK')
-
-apikey = '7DA0AN1D950FBI7VLR967QH3NK'
-secretkey = '3QMQQRTK3HSH6ISQ11UI3AGIPF'
-
-monitor = CustomMonitors.new(apikey, secretkey)
-
-#monitor.addMonitor()
